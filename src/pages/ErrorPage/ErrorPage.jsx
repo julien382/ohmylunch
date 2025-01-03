c0c514de56a46a0110ad7c6e3b11d08b3322037e
julien382 <julhermain@gmail.com>
2025-01-03 19:16:26 +0100

    create error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,15 @@
 import './ErrorPage.scss'
 
-const ErrorPage = () => {
+import { Link } from "react-router-dom";
 
+const ErrorPage = () => {
     return (
-        <div>
-            
+        <div className='errorPage'>
+            <h1 className='errorTitle'>404</h1>
+            <p className='errorMessage'>Page non trouvée</p>
+            <Link to="/" className='homeLink'>
+              <p className='logotexte'>{"Retour à l'accueil"}</p>
+            </Link>
         </div>
     )
 }
