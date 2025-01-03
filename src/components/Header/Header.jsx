afd96a693b9ded3ca94c95df3507801911b0c2fe
julien382 <julhermain@gmail.com>
2025-01-03 19:35:44 +0100

    create link header for back home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import './Header.scss'
 
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header>
-            <h1 className='nameLogo'>ohmylunch</h1>
+            <Link to="/">
+              <h1 className='nameLogo'>ohmylunch</h1>
+            </Link>
         </header>
     )
 }
