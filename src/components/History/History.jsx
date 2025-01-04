86973bacc71642cb218f7222844276ff83e333d4
julien382 <julhermain@gmail.com>
2025-01-04 17:29:35 +0100

    change css container history

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -4,7 +4,7 @@ const History = () => {
     return (
         <div className='history'>
                 <h2 className='historyTitle'>ohmylunch</h2>
-                <h3 className='historyText'>Notre Restaurant</h3>
+                <h3 className='historyText'>NOTRE RESTAURANT</h3>
                 <span className='laCarteTypePlatsBarre'></span>
 
         </div>
