2d7623acbac28c09cd974acbf0f6d107fa58b4ae
julien382 <julhermain@gmail.com>
2025-01-04 15:30:42 +0100

    add css and props LaCarteCard component

--- src/components/LaCarte/LaCarte.jsx
+++ src/components/LaCarte/LaCarte.jsx
@@ -2,6 +2,10 @@ import './LaCarte.scss'
 
 import LaCarteCard from '../LaCarteCard/LaCarteCard';
 
+import entree1 from '../../assets/img/plat.jpg';
+import entree2 from '../../assets/img/plat2.jpg';
+import entree3 from '../../assets/img/plat3.jpg';
+
 const LaCarte = () => {
 
     return (
@@ -13,9 +17,9 @@ const LaCarte = () => {
             <span className='laCarteTypePlatsBarre'></span>
 
             <div className='laCarteCards'>
-                <LaCarteCard />
-                <LaCarteCard />
-                <LaCarteCard />
+                <LaCarteCard img={entree1} title={"Fricassé d'escargot"} text={"Au piment d'espelette"} price={"25€"} />
+                <LaCarteCard img={entree2} title={"Foie gras de canard mi-cuit"} text={"Et ses copeaux de truffe noire"} price={"35€"}  />
+                <LaCarteCard img={entree3} title={"Oeuf au plat"} text={"Assaisonné à la truffe sur le lit de caviar"} price={"20€"}  />
             </div>
         </div>
     )
--- src/components/LaCarteCard/LaCarteCard.jsx
+++ src/components/LaCarteCard/LaCarteCard.jsx
@@ -1,21 +1,20 @@
 import './LaCarteCard.scss'
 
 import heart from '../../assets/svg/heart.svg';
-import plat from '../../assets/img/plat.jpg';
 
-const LaCarteCard = () => {
+const LaCarteCard = ({img, title, text, price}) => {
 
     return (
         <div className='laCarteCard'>
-            <img src={plat} alt='plat' />
+            <img src={img} alt='plat' />
             <div className='laCarteCardContent'>
                 <div className='laCarteCardText'>
-                    <h4>Salade César</h4>
-                    <p>Salade, poulet, tomates, parmesan, croûtons</p>
+                    <h4>{title}</h4>
+                    <p>{text}</p>
                 </div>
                 <div className='laCarteCardFooter'>
                     <img src={heart} alt="heart" />
-                    <p>12€</p>
+                    <p>{price}</p>
                 </div>
             </div>
         </div>
