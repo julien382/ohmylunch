17bca0f44bb9969ee3d7078f4168495d2ed02e5a
julien382 <julhermain@gmail.com>
2025-01-04 18:09:39 +0100

    create historyCardImages

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -14,23 +14,22 @@ const History = () => {
                 <div className='historyCards'>
                     <div className='historyCard'>
                         <img src={entree1} alt='plat' />
-                        <div className='historyCardContent'>
-                            <p>
-                                Plongez dans un univers où la passion culinaire rencontre un service chaleureux et une ambiance conviviale. Notre restaurant est une ode aux saveurs authentiques et aux moments de partage.
-                                Chaque plat raconte une histoire. Nous sélectionnons avec soin des ingrédients frais, locaux et de saison pour vous offrir une cuisine qui ravit les papilles et l’esprit. Que vous soyez amateur de [type de cuisine, ex. cuisine française raffinée, spécialités méditerranéennes, ou burgers gourmands], notre carte variée saura combler vos envies.
-                            </p>
-                        </div>
+                        <p className='historyCardContent'>
+                            Plongez dans un univers où la passion culinaire rencontre un service chaleureux et une ambiance conviviale. Notre restaurant est une ode aux saveurs authentiques et aux moments de partage.
+                            Chaque plat raconte une histoire. Nous sélectionnons avec soin des ingrédients frais, locaux et de saison pour vous offrir une cuisine qui ravit les papilles et l’esprit. Que vous soyez amateur de [type de cuisine, ex. cuisine française raffinée, spécialités méditerranéennes, ou burgers gourmands], notre carte variée saura combler vos envies.
+                        </p>
                     </div>
 
-
+                    <div className='historyCardImages'>
+                        <img src={entree2} alt='plat' />
+                        <img src={entree3} alt='plat' />
+                    </div>
 
                     <div className='historyCard'>
-                        <img src={entree2} alt='plat' />
-                        <div className='historyCardContent'>
-                            <p>
+                        <p className='historyCardContent'>
                                 Notre équipe est une véritable symphonie de talents où chaque métier joue un rôle essentiel : en cuisine, nos chefs et commis travaillent avec passion pour créer des plats savoureux et artistiques ; en salle, nos serveurs et responsables veillent à offrir un service impeccable et attentionné ; et en coulisses, nos gestionnaires, plongeurs et autres collaborateurs contribuent à faire fonctionner l’ensemble avec fluidité. Ensemble, nous partageons une même ambition : vous offrir une expérience culinaire mémorable, où professionnalisme, convivialité et passion se rencontrent.
-                            </p>
-                        </div>
+                        </p>
+                        <img src={entree2} alt='plat' />
                     </div>
 
                 </div>
