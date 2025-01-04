66d7de108cdaf001ddc699359934022948bbd6f1
julien382 <julhermain@gmail.com>
2025-01-04 18:38:37 +0100

    add arrowLeft back home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import './Header.scss'
 
+import arrowLeft from '../../assets/svg/arrowLeft.svg';
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
             <Link to="/">
-              <h1 className='nameLogo'>ohmylunch</h1>
+                <img src={arrowLeft} className='arrowLeft' alt='arrowLeft' />
+            </Link>
+            <Link to="/">
+                <h1 className='nameLogo'>ohmylunch</h1>
             </Link>
         </header>
     )
