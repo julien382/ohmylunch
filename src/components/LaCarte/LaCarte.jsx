ea96ede71bdd2f1120762b7c24c807f97766654b
julien382 <julhermain@gmail.com>
2025-01-04 15:49:05 +0100

    add data plats desserts

--- src/components/LaCarte/LaCarte.jsx
+++ src/components/LaCarte/LaCarte.jsx
@@ -25,26 +25,26 @@ const LaCarte = () => {
             </div>
 
             <div className='laCarteTypePlatsContainer'>
-                <h3 className='laCarteTypePlats'>ENTREES</h3>
+                <h3 className='laCarteTypePlats'>PLATS</h3>
 
                 <span className='laCarteTypePlatsBarre'></span>
 
                 <div className='laCarteCards'>
-                    <LaCarteCard img={entree1} title={"Fricassé d'escargot"} text={"Au piment d'espelette"} price={"25€"} />
-                    <LaCarteCard img={entree2} title={"Foie gras de canard mi-cuit"} text={"Et ses copeaux de truffe noire"} price={"35€"}  />
-                    <LaCarteCard img={entree3} title={"Oeuf au plat"} text={"Assaisonné à la truffe sur le lit de caviar"} price={"20€"}  />
+                    <LaCarteCard img={entree1} title={"Filet de boeuf aux herbes"} text={"Accompagné de sa ribambelle de légumes"} price={"40€"} />
+                    <LaCarteCard img={entree2} title={"Parmentier de queue de boeuf"} text={"À la truffe noir sur sa purée de panais"} price={"35€"}  />
+                    <LaCarteCard img={entree3} title={"Filet du Turbot"} text={"Aux agrumes"} price={"44€"}  />
                 </div>
             </div>
 
             <div className='laCarteTypePlatsContainer'>
-                <h3 className='laCarteTypePlats'>ENTREES</h3>
+                <h3 className='laCarteTypePlats'>DESSERTS</h3>
 
                 <span className='laCarteTypePlatsBarre'></span>
 
                 <div className='laCarteCards'>
-                    <LaCarteCard img={entree1} title={"Fricassé d'escargot"} text={"Au piment d'espelette"} price={"25€"} />
-                    <LaCarteCard img={entree2} title={"Foie gras de canard mi-cuit"} text={"Et ses copeaux de truffe noire"} price={"35€"}  />
-                    <LaCarteCard img={entree3} title={"Oeuf au plat"} text={"Assaisonné à la truffe sur le lit de caviar"} price={"20€"}  />
+                    <LaCarteCard img={entree1} title={"Paris-Brest"} text={"Revisité"} price={"18€"} />
+                    <LaCarteCard img={entree2} title={"Macaron au chocolat d'exception"} text={"Et glace à la vanille de Madagascar"} price={"22€"}  />
+                    <LaCarteCard img={entree3} title={"Mousse au chocolat"} text={"Au piment d'espelette à la truffe noire"} price={"23€"}  />
                 </div>
             </div>
 
