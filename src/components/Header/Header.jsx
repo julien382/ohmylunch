1ac311b3bf77cde25d631e3818f9a4c9058f2cc3
julien382 <julhermain@gmail.com>
2025-01-04 18:53:30 +0100

    add condition for delete arrow in mainPage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
 import './Header.scss'
 
 import arrowLeft from '../../assets/svg/arrowLeft.svg';
-
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+    const location = useLocation();
+    const showArrow = location.pathname !== "/";
+
     return (
         <header>
-            <Link to="/">
-                <img src={arrowLeft} className='arrowLeft' alt='arrowLeft' />
-            </Link>
+            {showArrow && (
+                <Link to="/">
+                    <img src={arrowLeft} className='arrowLeft' alt='arrowLeft' />
+                </Link>
+            )}
             <Link to="/">
                 <h1 className='nameLogo'>ohmylunch</h1>
             </Link>
