9347e425b3d7a551227dfea416daeb26e0c39b74
julien382 <julhermain@gmail.com>
2025-01-06 16:14:18 +0100

    create json Menu

--- src/components/LaCarte/LaCarte.jsx
+++ src/components/LaCarte/LaCarte.jsx
@@ -1,61 +1,78 @@
-import './LaCarte.scss'
-
+import './LaCarte.scss';
 import LaCarteCard from '../LaCarteCard/LaCarteCard';
-
-import entree1 from '../../assets/img/plat.jpg';
-import entree2 from '../../assets/img/plat2.jpg';
-import entree3 from '../../assets/img/plat3.jpg';
-
+import data from '../../data/dataMenu.json'; // Importer les données JSON
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 const LaCarte = () => {
+  // Créer un état pour stocker les chemins d'images résolus
+  const [images, setImages] = useState({});
 
-    return (
-        <div className='laCarte'>
-            <h2 className='laCarteTitle'>La Carte</h2>
+  // Utiliser useEffect pour importer les images au chargement du composant
+  useEffect(() => {
+    const loadImages = async () => {
+      const imageImports = {};
 
-            <div className='laCarteTypePlatsContainer'>
-                <h3 className='laCarteTypePlats'>ENTREES</h3>
+      // Charger les images dynamiquement et les stocker dans l'état
+      for (let card of data) {
+        const imgPath = `../../assets/img/${card.img}.jpg`;
+        imageImports[card.img] = (await import(`${imgPath}`)).default;
+      }
 
-                <span className='laCarteTypePlatsBarre'></span>
+      setImages(imageImports);
+    };
 
-                <div className='laCarteCards'>
-                    <LaCarteCard img={entree1} title={"Fricassé d'escargot"} text={"Au piment d'espelette"} price={"25€"} />
-                    <LaCarteCard img={entree2} title={"Foie gras de canard mi-cuit"} text={"Et ses copeaux de truffe noire"} price={"35€"}  />
-                    <LaCarteCard img={entree3} title={"Oeuf au plat"} text={"Assaisonné à la truffe sur le lit de caviar"} price={"20€"}  />
-                </div>
-            </div>
+    loadImages();
+  }, []); // Ce useEffect ne s'exécute qu'une seule fois, au montage du composant
 
-            <div className='laCarteTypePlatsContainer'>
-                <h3 className='laCarteTypePlats'>PLATS</h3>
+  return (
+    <div className='laCarte'>
+      <h2 className='laCarteTitle'>La Carte</h2>
 
-                <span className='laCarteTypePlatsBarre'></span>
+      <div className='laCarteTypePlatsContainer'>
+        <h3 className='laCarteTypePlats'>ENTREES</h3>
+        <span className='laCarteTypePlatsBarre'></span>
 
-                <div className='laCarteCards'>
-                    <LaCarteCard img={entree1} title={"Filet de boeuf aux herbes"} text={"Accompagné de sa ribambelle de légumes"} price={"40€"} />
-                    <LaCarteCard img={entree2} title={"Parmentier de queue de boeuf"} text={"À la truffe noir sur sa purée de panais"} price={"35€"}  />
-                    <LaCarteCard img={entree3} title={"Filet du Turbot"} text={"Aux agrumes"} price={"44€"}  />
-                </div>
-            </div>
-
-            <div className='laCarteTypePlatsContainer'>
-                <h3 className='laCarteTypePlats'>DESSERTS</h3>
-
-                <span className='laCarteTypePlatsBarre'></span>
-
-                <div className='laCarteCards'>
-                    <LaCarteCard img={entree1} title={"Paris-Brest"} text={"Revisité"} price={"18€"} />
-                    <LaCarteCard img={entree2} title={"Macaron au chocolat d'exception"} text={"Et glace à la vanille de Madagascar"} price={"22€"}  />
-                    <LaCarteCard img={entree3} title={"Mousse au chocolat"} text={"Au piment d'espelette à la truffe noire"} price={"23€"}  />
-                </div>
-            </div>
-
-            <Link to="/Panier" className='buttonCommander'>
-              <p>Commander</p>
-            </Link>
-
+        <div className='laCarteCards'>
+          {data.map((card, index) => (
+            <LaCarteCard
+              key={index}
+              img={images[card.img]}  // Utiliser l'image chargée dynamiquement
+              title={card.title}
+              text={card.text}
+              price={card.price}
+            />
+          ))}
         </div>
-    )
+      </div>
+
+      <div className='laCarteTypePlatsContainer'>
+        <h3 className='laCarteTypePlats'>PLATS</h3>
+        <span className='laCarteTypePlatsBarre'></span>
+
+        <div className='laCarteCards'>
+          <LaCarteCard img={images['plat']} title={"Filet de boeuf aux herbes"} text={"Accompagné de sa ribambelle de légumes"} price={"40€"} />
+          <LaCarteCard img={images['plat2']} title={"Parmentier de queue de boeuf"} text={"À la truffe noir sur sa purée de panais"} price={"35€"}  />
+          <LaCarteCard img={images['plat3']} title={"Filet du Turbot"} text={"Aux agrumes"} price={"44€"}  />
+        </div>
+      </div>
+
+      <div className='laCarteTypePlatsContainer'>
+        <h3 className='laCarteTypePlats'>DESSERTS</h3>
+        <span className='laCarteTypePlatsBarre'></span>
+
+        <div className='laCarteCards'>
+          <LaCarteCard img={images['plat']} title={"Paris-Brest"} text={"Revisité"} price={"18€"} />
+          <LaCarteCard img={images['plat2']} title={"Macaron au chocolat d'exception"} text={"Et glace à la vanille de Madagascar"} price={"22€"}  />
+          <LaCarteCard img={images['plat3']} title={"Mousse au chocolat"} text={"Au piment d'espelette à la truffe noire"} price={"23€"}  />
+        </div>
+      </div>
+
+      <Link to="/Panier" className='buttonCommander'>
+        <p>Commander</p>
+      </Link>
+    </div>
+  );
 }
 
-export default LaCarte
+export default LaCarte;
