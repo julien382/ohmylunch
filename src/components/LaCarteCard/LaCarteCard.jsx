44f75d0f55832a7ff223d5cc830915e5aa1edbad
julien382 <julhermain@gmail.com>
2025-01-06 14:18:54 +0100

    add color heart

--- src/components/LaCarteCard/LaCarteCard.jsx
+++ src/components/LaCarteCard/LaCarteCard.jsx
@@ -1,7 +1,4 @@
 import './LaCarteCard.scss'
-
-import heart from '../../assets/svg/heart.svg';
-import filledHeart from '../../assets/svg/heartFull.svg';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
@@ -21,12 +18,18 @@ const LaCarteCard = ({img, title, text, price}) => {
                     <p>{text}</p>
                 </div>
                 <div className='laCarteCardFooter'>
-                    <img 
-                        src={isFavorite ? filledHeart : heart} 
-                        alt="heart" 
-                        onClick={toggleFavorite} 
-                        className='heartIcon'
-                    />
+                    <svg
+                        className={`heartIcon ${isFavorite ? 'filled' : ''}`}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        onClick={toggleFavorite}
+                    >
+                        <path
+                            d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                        />
+                    </svg>
                     <p>{price}</p>
                 </div>
             </div>
@@ -44,4 +47,4 @@ LaCarteCard.propTypes = {
     ]).isRequired,
 };
 
-export default LaCarteCard
+export default LaCarteCard;
