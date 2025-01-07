6d3ac21b3ffbaf2cb563b4687be3fe138a126727
julien382 <julhermain@gmail.com>
2025-01-07 18:56:16 +0100

    fonctionnel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  homepage: "https://julien382.github.io/ohmylunch/",
-  plugins: [react()]
+  plugins: [react()],
+  
+  base: '/ohmylunch/',
 })
