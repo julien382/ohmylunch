41d83ff969ee51c12c1849cb51bbd90276228c30
julien382 <julhermain@gmail.com>
2025-01-07 23:31:07 +0100

    change toggleFavorite for lacarteCard

--- src/components/LaCarteCard/LaCarteCard.jsx
+++ src/components/LaCarteCard/LaCarteCard.jsx
@@ -9,7 +9,7 @@ const LaCarteCard = ({ img, title, text, price, onToggleFavorite, isFavorite }) 
     };
 
     return (
-        <div className='laCarteCard'>
+        <div className='laCarteCard' onClick={toggleFavorite}>
             <img src={img} alt={title} className='laCarteCardImage' /> {/* Utilisation de la prop img ici */}
             <div className='laCarteCardContent'>
                 <div className='laCarteCardText'>
@@ -23,7 +23,6 @@ const LaCarteCard = ({ img, title, text, price, onToggleFavorite, isFavorite }) 
                         width="24"
                         height="24"
                         viewBox="0 0 24 24"
-                        onClick={toggleFavorite}
                     >
                         <path
                             d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
