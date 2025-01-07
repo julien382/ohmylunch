9f3d8c1c1093a22c53fe235cdebc7c5682f8b499
julien382 <julhermain@gmail.com>
2025-01-07 12:31:38 +0100

    fix bug build dist

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ohmylunch/',
   plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
+  base: '/ohmylunch/',
 })
